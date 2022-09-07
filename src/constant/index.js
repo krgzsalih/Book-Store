f4465b7ff294525abed519cb86ed5f472bc23499
Vedat Eroglu <vdterog@gmail.com>
2022-09-07 12:34:31 +0300

    delete constant

--- src/constant/index.js
+++
@@ -1,6 +0,0 @@
-export const API_KEY = "&key=AIzaSyDIJGJLUi0G129NVjeFr6uul-8xCK0hT2c"
-export const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
-
-
-
-//list	GET  /volumes?q={search terms}	Performs a book search.
