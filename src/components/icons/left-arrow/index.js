2cf0970f60099fbb044baf0a8d50c5bca4875714
krgzsalih <krgzahmetsalih@gmail.com>
2022-09-12 14:19:12 +0300

    update info

---
+++ src/components/icons/left-arrow/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { MDBIcon } from "mdb-react-ui-kit";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+
+const LeftArrow = () => {
+  return (
+    <>
+      <MDBIcon fas icon="arrow-left" />
+      <MDBIcon fas icon="chevron-circle-left" />
+    </>
+  );
+};
+
+export default LeftArrow;
--- src/components/update-info/index.js
+++ src/components/update-info/index.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { useData } from '../../context/use-data'
+import React from "react";
+import { useData } from "../../context/use-data";
+import LeftArrow from "../icons/left-arrow";
+import Style from "./style.module.scss";
 
 const UpdateInfo = () => {
+  const { bookInfo, setInfo } = useData();
 
-    const { bookInfo, setInfo } = useData()
+  return (
+    <div className={Style.container}>
+      <div>
+        <LeftArrow />
+        <button className={Style.backBttn} onClick={() => setInfo(false)}>Back</button>
+      </div>
+      <div>
+        <img src={bookInfo.thumbnail}></img>
+      </div>
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-            <img src={bookInfo.thumbnail}></img>
-            <button onClick={() => setInfo(false)}>Back</button>
-        </div>
-    )
-}
-
-export default UpdateInfo
+export default UpdateInfo;
