d7571e91027d9fee20f08cd9fd65eda4095eff8f
krgzsalih <krgzahmetsalih@gmail.com>
2022-09-14 20:46:15 +0300

    category bugs fixed

--- src/components/add-options/index.js
+++ src/components/add-options/index.js
@@ -28,7 +28,7 @@ const AddOption = (props) => {
                         description: `${item.volumeInfo.description || "None"}`,
                         count: `${count}`,
                         price: `${price}`,
-                        category:`${item.volumeInfo.categories.map(x => x) || "None"}`
+                        category:`${item.volumeInfo.categories ? item.volumeInfo.categories.map(x => x) : "None"}`
                     }
                 },
                 {
--- src/constants/axios.js
+++ src/constants/axios.js
@@ -3,7 +3,6 @@
 
 // GOOGLE API ========>
 export const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
-export const CATEGORY_URL = "https://www.googleapis.com/books/v1/volumes?q="
 export const API_KEY = "&key=AIzaSyAEc8QDLPW_uoPsTDBfQWW11TGEsJj4G8Q"
 
 // LOCAL API ========>
